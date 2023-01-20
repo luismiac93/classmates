f6306be26e67c5316c0339b5c82faa373f2347b2
Luismi <luismiac93@outlook.com>
2023-01-21 09:54:21 +1100

    update font size, bug in random table and Jin's name

--- src/pages/HomeScreen.tsx
+++ src/pages/HomeScreen.tsx
@@ -3,34 +3,43 @@ import "animate.css";
 
 export const HomeScreen = () => {
   const [classmates] = useState([
-    "Luis",
-    "Fede",
-    "Palm",
-    "Monti",
-    "April",
-    "Victoria",
-    "Shin Young (신영)",
-    "Eiji",
-    "Heeseo (희서)",
-    "Simge",
-    "Arturo",
-    "Jin (진)",
-    "Grace",
+    { name: "Luis", active: true },
+    { name: "Fede", active: true },
+    { name: "Palm", active: true },
+    { name: "Monti", active: true },
+    { name: "April", active: true },
+    { name: "Victoria", active: true },
+    { name: "Shin Young (신영)", active: true },
+    { name: "Eiji", active: true },
+    { name: "Heeseo (희서)", active: true },
+    { name: "Simge", active: true },
+    { name: "Arturo", active: true },
+    { name: "Jin (현진)", active: true },
+    { name: "Grace", active: true },
   ]);
   const [grups, setGroups] = useState<
     Array<{ name: string; classmates: Array<string> }>
   >([]);
 
   const generatorClassmates = () => {
-    let tempClassmates = [...classmates];
+    let tempClassmates = classmates.filter((x) => x.active === true);
     let tempGroup: Array<string[]> = [[], [], []];
-    let countGroup = 0;
+    let countGroup = [0, 1, 2];
     while (tempClassmates.length > 0) {
+      if (countGroup.length === 0) {
+        countGroup = [0, 1, 2];
+      }
+
       let classmate =
         tempClassmates[Math.floor(Math.random() * tempClassmates.length)];
-      tempClassmates = tempClassmates.filter((x) => x !== classmate);
-      tempGroup[countGroup].push(classmate);
-      countGroup = countGroup === 2 ? 0 : countGroup + 1;
+
+      tempClassmates = tempClassmates.filter((x) => x.name !== classmate.name);
+
+      let group = countGroup[Math.floor(Math.random() * countGroup.length)];
+
+      countGroup = countGroup.filter((x) => x !== group);
+
+      tempGroup[group].push(classmate.name);
     }
 
     let dataFinal = [
@@ -60,12 +69,11 @@ export const HomeScreen = () => {
                     {item.name}
                     <br />
                     <br />
-                    {item.classmates.map(
-                      (e, i) =>
-                        `${i + 1}. ${e}${
-                          i === item.classmates.length - 1 ? "." : ","
-                        } \n`
-                    )}
+                    {item.classmates.map((e, i) => (
+                      <div key={e}>
+                        {i + 1}. {e}.
+                      </div>
+                    ))}
                   </div>
                 </div>
                 <div className="holes hole-top"></div>
